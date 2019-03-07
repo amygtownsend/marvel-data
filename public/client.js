0232a4e016aa609ac6f6da463726e5380f458ffe
Amy Townsend <amygtownsend@gmail.com>
2019-03-07 03:20:40 -0500

    Add Chart.js and Marvel API authorization and calls

--- public/client.js
+++ public/client.js
@@ -30,7 +30,136 @@ let init = function () {
 
   // CHART.JS
 
-  // Add Chart.js code...
+  setTimeout(function () {
+    fetch('/character').then(resp => resp.json()).then((data) => {
+      console.log(data);
+
+      let comicsAll = [];
+      let eventsAll = [];
+
+      data.forEach((group) => {
+        group.forEach((character) => {
+          comicsAll.push(character);
+          eventsAll.push(character);
+        });
+      });
+
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+      function names(a, b) {
+        var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+        var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+        return 0;
+      }
+
+      function comics(a, b) {
+        return b.comics.available - a.comics.available;
+      }
+
+      function events(a, b) {
+        return b.events.available - a.events.available;
+      }
+
+      comicsAll.sort(comics);
+      eventsAll.sort(events);
+
+      let comicsShort = comicsAll.slice(0, 30);
+      let eventsShort = eventsAll.slice(0, 30);
+
+      // https://stackoverflow.com/questions/1584370/how-to-merge-two-arrays-in-javascript-and-de-duplicate-items
+      function arrayUnique(array) {
+        var a = array.concat();
+        for (var i = 0; i < a.length; ++i) {
+          for (var j = i + 1; j < a.length; ++j) {
+            if (a[i] === a[j]) {
+              a.splice(j--, 1);
+            }
+          }
+        }
+
+        return a;
+      }
+
+      // Merges both arrays and gets unique items
+      let joined = arrayUnique(comicsShort.concat(eventsShort));
+      let scatterCharacters = joined.sort(names);
+      console.log(scatterCharacters);
+
+      let scatterLabels = [];
+      let scatterData = [];
+
+      scatterCharacters.forEach((character) => {
+        scatterLabels.push(character.name);
+        scatterData.push({ x: character.events.available, y: character.comics.available });
+      });
+
+      let ctx = document.getElementById("chart").getContext('2d');
+      Chart.defaults.global.elements.point.radius = 5;
+      Chart.defaults.global.elements.point.hoverRadius = 6;
+      Chart.defaults.global.defaultFontFamily = "'Overpass', sans-serif";
+      Chart.defaults.global.defaultFontColor = '#22262a';
+      var scatterChart = new Chart(ctx, {
+        type: 'scatter',
+        data: {
+          labels: scatterLabels,
+          datasets: [{
+            label: 'Marvel Characters',
+            data: scatterData,
+            backgroundColor: 'rgba(237, 29, 36, .5)',
+            borderColor: 'rgba(237, 29, 36)',
+            borderWidth: 1
+          }]
+        },
+        options: {
+          aspectRatio: 1.5,
+          legend: {
+            display: false
+          },
+          title: {
+            display: true,
+            fontSize: '16',
+            text: 'Marvel Characters: Comics and Events'
+          },
+          scales: {
+            xAxes: [{
+              type: 'linear',
+              position: 'bottom',
+              scaleLabel: {
+                labelString: '# of Events',
+                display: true,
+                fontColor: 'rgb(237, 29, 36)',
+                fontStyle: 'bold'
+              }
+            }],
+            yAxes: [{
+              type: 'linear',
+              scaleLabel: {
+                labelString: '# of Comics',
+                display: true,
+                fontColor: 'rgb(237, 29, 36)',
+                fontStyle: 'bold'
+              }
+            }]
+          },
+          // https://stackoverflow.com/questions/44661671/chart-js-scatter-chart-displaying-label-specific-to-point-in-tooltip
+          tooltips: {
+            callbacks: {
+              label: function (tooltipItem, data) {
+                var label = data.labels[tooltipItem.index];
+                return label + ': (' + tooltipItem.xLabel + ' comics, ' + tooltipItem.yLabel + ' events)';
+              }
+            }
+          }
+        }
+      });
+
+    });
+  }, 300);
 }
 
 document.addEventListener("DOMContentLoaded", init);
--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 // server.js
 
-// init project
-var express = require('express');
-var app = express();
+// imports
+const dotenv = require('dotenv');
+const crypto = require('crypto');
+const express = require('express');
+const https = require('https');
 
-var bodyParser = require('body-parser');
+dotenv.config();
+
+// globals
+const app = express();
+const publicKey = process.env.PUBLIC_KEY;
+const privateKey = process.env.PRIVATE_KEY;
+const baseURL = "https://gateway.marvel.com:443/v1/public/";
+
+let characterData = []; // to cache our API response
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
-app.use(bodyParser.json());
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
@@ -17,12 +26,70 @@ app.get("/", function (request, response) {
 
 
 //-------------------------------------------------------------//
+//----------------------- AUTHORIZATION -----------------------//
+//-------------------------------------------------------------//
+
+
+/** Make a marvel-api compliant hash.
+  * https://developer.marvel.com/documentation/authorization
+  * https://gist.github.com/kitek/1579117
+  */
+
+function makeHash() {
+  let ts = new Date().getTime();
+  var data = `${ts}${privateKey}${publicKey}`;
+  let hash = crypto.createHash('md5').update(data).digest("hex");
+  return { ts, hash };
+}
+
+
+//-------------------------------------------------------------//
+//------------------------- API CALLS -------------------------//
+//-------------------------------------------------------------//
+
+app.get('/character', (request, response) => {
+  // Only fetch data if it's not cached in a var already
+  if (!characterData || characterData.length < 75) {
+    // Make the API hash
+    let hash = makeHash();
+
+    // Loop through all character data
+    for (let offset = 0; offset <= 1480; offset += 20) {
+      let charURL = `${baseURL}characters?offset=${offset}&apikey=${publicKey}&ts=${hash.ts}&hash=${hash.hash}`;
+
+      // Fetch the url with a GET request
+      https.get(charURL, (res) => {
+        // Make empty string to add data to as the response streams in
+        let str = "";
+        res.on('data', function (chunk) {
+          str += chunk;
+        });
+
+        // When the response is finished, cache results in characterData array
+        res.on('end', function () {
+          // Cache the data
+          let jsonData = JSON.parse(str);
+          characterData.push(jsonData.data.results);
+          // Return the results when for loop finishes
+          if (characterData.length == 75) {
+            console.log("API call");
+            response.send(characterData);
+          }
+        });
+      });
+    };
+  } else {
+    console.log("Cached data");
+    response.send(characterData);
+  }
+});
+
+//-------------------------------------------------------------//
 //------------------------ WEB SERVER -------------------------//
 //-------------------------------------------------------------//
 
 
-// Listen for requests to our app
-// We make these requests from client.js
+// Listen for requests
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
